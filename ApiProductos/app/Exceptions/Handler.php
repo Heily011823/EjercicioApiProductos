a73677c6d13ac8a15c1de9e5c6995b76ba9807b7
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-03-12 18:05:04 -0500

    fix: ajustes en middleware Authenticate y manejo de excepciones en Handler

--- ApiProductos/app/Exceptions/Handler.php
+++ ApiProductos/app/Exceptions/Handler.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Exceptions;
-
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,12 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json([
+            'message' => 'Unauthenticated.'
+        ], 401);
     }
 }
